1009ed5627d8080a1c9d1b1e7d1f68eb16456dce
Duy Do <anhduy29121998@gmail.com>
2022-06-13 05:58:57 +0700

    Not merge a Phuc
    Demo noti
    
    Reload list after add record

--- app/src/main/java/com/example/moneyaah/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.example.moneyaah;
-
-public class Constants {
-    public final static String USERNAME = "USERNAME";
-    public final static String TAG = "EmailPassword";
-    public final static String NAVIGATE_STRING = "NAVIGATE_STRING";
-    public final static String USER_CURRENT_AMOUNT = "USER_AMOUNT";
-}
